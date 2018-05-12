67bed257ddf9d0738813d96689d4f16668046a5e
Janice Darikho <janicedarikho@gmail.com>
2018-05-12 03:03:26 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -45,8 +45,13 @@ def play(board)
 end
 
 def turn_count(board)
+  counter = 0
   board.each do |space| 
-    
+    if space == "X" || space == "O"
+      counter += 1
+    else
+      counter += 0
+    end
   end
 end
 
